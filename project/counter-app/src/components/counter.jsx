11a2adead782fff65e264334effe35c53ce68641
pratham choudhary <prathamchoudhary27037@gmail.com>
2020-08-26 22:30:48 +0530

    initial demo project

---
+++ project/counter-app/src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <button>increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
