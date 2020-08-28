37bb54cf204c008362b0976d7417508c6711af46
pratham choudhary <prathamchoudhary27037@gmail.com>
2020-08-28 22:47:15 +0530

    almost half way

--- project/counter-app/src/components/counter.jsx
+++ project/counter-app/src/components/counter.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>there are no tags</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div>
-        <h1>hello world</h1>
-        <button>increment</button>
+        {this.state.tags.length === 0 && "no tag"}
+        {this.renderTags()}
+        <span>{this.state.count}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
